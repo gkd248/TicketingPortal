ac564d217f42612a461d37af63eac4c055fa270c
gkd248 <gdarnell@utexas.edu>
2021-04-08 22:57:34 -0500

    Basic webpage with buttons, no backend integration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import ticket from './ticket.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>Ticketing Service</h1>
+
+      <table>
+        <tr>
+          <td><button className="buttonGreen" type="submit">Create Event</button></td>
+          <td><button className="buttonBlue" type="submit">Buy Ticket</button></td>
+          <td><button className="buttonRed" type="submit">Something</button></td>
+        </tr>
+        <tr>
+          <td><button className="buttonBlack" type="submit">Cancel Event</button></td>
+          <td><button className="buttonGray" type="submit">Transfer Ticket</button></td>
+          <td><button className="buttonGreen" type="submit">Something</button></td>
+        </tr>
+      </table>
+
+      <br></br><br></br>
+
+      <img src={ticket} className="App-logo" alt="logo" />
+
       </header>
     </div>
   );
