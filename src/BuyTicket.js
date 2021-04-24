fe0b1de15ea84b95e0b145ea7258c2f5287ca473
gkd248 <gdarnell@utexas.edu>
2021-04-23 19:03:42 -0500

    Added loadBlockchain functions and createEvent web3 method and SmartContractABI.js

--- src/BuyTicket.js
+++ src/BuyTicket.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import { Card, ListGroup, Form, Button } from 'react-bootstrap';
+import Web3 from 'web3'
+import {EVENT_CONTRACT_ABI, EVENT_CONTRACT_ADDRESS} from "./SmartContractABI.js"
 
 class BuyTicket extends Component {
+
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    const account = accounts[0];
+    const contract = await new web3.eth.Contract(EVENT_CONTRACT_ABI, EVENT_CONTRACT_ADDRESS, {from: account, gas: 1500000, gasPrice: '20000000000'})
+    //const events = await web3.eth.get
+    this.setState({ account: account , web3: web3, contract: contract})
+  }
+
+  componentDidMount() {
+    this.loadBlockchainData()
+  }
+
     render() {
         return (
           <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
--- src/CreateEvent.js
+++ src/CreateEvent.js
@@ -1,7 +1,60 @@
 import React, { Component } from 'react';
 import { Card, ListGroup, Form, Button } from 'react-bootstrap';
+import Web3 from 'web3'
+import {EVENT_CONTRACT_ABI, EVENT_CONTRACT_ADDRESS} from "./SmartContractABI.js"
 
 class CreateEvent extends Component {
+
+      async loadBlockchainData() {
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+        const accounts = await web3.eth.getAccounts()
+        const account = accounts[0];
+        const contract = await new web3.eth.Contract(EVENT_CONTRACT_ABI, EVENT_CONTRACT_ADDRESS, {from: account, gas: 1500000, gasPrice: '20000000000'})
+        //const events = await web3.eth.get
+        this.setState({ account: account , web3: web3, contract: contract})
+      }
+
+      componentDidMount() {
+        this.loadBlockchainData()
+      }
+
+      contractCreateEvent(event) {
+        this.enableMetamask();
+        let token_uri =  {
+          "event_name": this.state.event_name,
+          "price": this.state.event_price,
+          "date": this.state.date,
+          "start_time": this.state.start_time,
+          "end_time": this.state.end_time,
+          "version": 1
+        };
+        let capacity = this.state.event_capacity;
+        token_uri["event_creator"] = this.state.account;
+        console.log("createEvent")
+        let tokenID = this.getTokenID(token_uri);
+        try {
+          this.state.contract.methods.mintWithTokenURI(capacity, tokenID, JSON.stringify(token_uri), this.state.event_price).send({'from': this.state.account})
+          .on('transactionHash', function(hash){
+            console.log("TransactionHash " + hash);
+            this.setState({createdEventID: "Created Event TokenID: " + tokenID});
+          }.bind(this))
+            .on('receipt', function(receipt){
+              console.log("Receipt " + JSON.stringify(receipt));
+            })
+            .on('confirmation', function(confirmationNumber, receipt){
+              console.log("Confirmation Number " + confirmationNumber);
+            })
+            .on('error', console.error);
+          } catch (error) {
+          console.log("Error" + error)
+        }
+        event.preventDefault();
+      }
+
+      enableMetamask = ()  => {
+      window.ethereum.enable();
+      }
+
     render() {
         return (
             <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
